d0d4df27dd70829c8538957eda5054516b3644d1
Δionysus βenstein <34517958+DionysusBenstein@users.noreply.github.com>
2021-05-13 10:23:05 +0300

    Update deploy-proxy-centos7.sh

--- deploy-proxy-centos7.sh
+++ deploy-proxy-centos7.sh
@@ -9,4 +9,6 @@ git clone https://github.com/elejke/docker-socks5.git
 cd docker-socks5
 sudo docker build -t socks5 .
 sudo docker run -d -p $((1024 + RANDOM % 65535)):1080 socks5
+
+hostname -I | awk '{print $1}'
 sudo docker ps | awk 'FNR==2{print $11}' | sed 's/^0.0.0.0://'
